86a1532a905a23fabf6688ae1a245d9a72ebaee2
Wuruohai <w3035675429@163.com>
2024-06-11 16:47:16 +0800

    第一次提交

---
+++ gameking/pages/webview/webview.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<button @click="testco()">请求云对象方法</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods:{
+			async testco(){
+				const helloco = uniCloud.importObject('helloco')
+				try {
+					const res = await helloco.sum(1,2)
+					console.log(res)
+				} catch (e){
+					console.log(e)
+				}
+			}
+		}
+	}
+</script>
+
+
